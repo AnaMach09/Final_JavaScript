6c94a2ab24c84d7dbefea0c9a89a18b3067a1de1
AnaMach09 <machavarianiana0@gmail.com>
2022-12-08 21:40:54 +0400

    finalproject

---
+++ fetch.js
@@ -0,0 +1,38 @@
+"use strict"
+let result=document.getElementById("results");
+let filter=document.getElementById("filter");
+let listItems= [];
+
+getData();
+
+async function getData(){
+    const res=await fetch ("https://randomuser.me/api/?results=10")
+    const {results}=await res.json()
+
+   result.innerHTML=" "
+
+   results.forEach(members => {
+    const li=document.createElement("li")
+    listItems.push(li)
+    li.innerHTML=`
+    <img src="${members.picture.large}" alt=${members.name.first}"
+    <div class="members-info">
+    <h4>${members.name.first} ${members.name.last}</h4>
+    </div>
+    `
+    result.appendChild(li)
+   });
+}
+
+function filterData(searchItem){
+    listItems.forEach(item =>{
+        if(item.innerText.toLowerCase().includes(searchItem.toLowerCase())){
+            item.classList.remove("hide")
+        }else{
+            item.classList.add("hide")
+        }
+    })
+}
+
+
+filter.addEventListener("keyup",(element) =>filterData(element.target.value));
---
+++ scroll.js
@@ -0,0 +1,4 @@
+// Get the element
+let topBtn = document.querySelector(".top-btn");
+topBtn.onclick = () => window.scrollTo({ top: 0, behavior: "smooth" });
+window.onscroll = () => window.scrollY > 500 ? topBtn.style.opacity = 1 : topBtn.style.opacity = 0
